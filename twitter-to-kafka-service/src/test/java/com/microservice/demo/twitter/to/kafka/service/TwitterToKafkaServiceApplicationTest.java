6a1c30cbc941955030104846f656386a3ff6d77d
andr30z <andrelp1015@gmail.com>
2022-05-20 22:14:35 -0300

    adding build image commands to twitter-to-kafka-service module

--- twitter-to-kafka-service/src/test/java/com/microservice/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplicationTest.java
+++ twitter-to-kafka-service/src/test/java/com/microservice/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplicationTest.java
@@ -3,7 +3,7 @@ package com.microservice.demo.twitter.to.kafka.service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = TwitterToKafkaServiceApplicationTest.class)
 public class TwitterToKafkaServiceApplicationTest {
     @Test
     public void contextLoad() {
